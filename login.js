437122aeb7479c1484df53d52a3fb1417efdee5e
OseMine <oskar.wiedrich@gmail.com>
2022-04-30 10:54:40 +0200

    Hello

---
+++ login.js
@@ -0,0 +1,5 @@
+function getInfo() {
+    var username = document.getElementById("...")
+    var password = document.getElementById("...")
+    console.log("your username is" + username + "and your password is" + password)
+}
